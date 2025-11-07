09247e151df7d6f766ba46c68aae871dd6cef0e6
Mariano Hurtado de Mendoza Carranza <mariano.hmc@hotmail.com>
2025-11-07 13:07:24 -0600

    Can get all tasks from the table

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,44 +2,33 @@ import "./App.css"
 import Button from "./components/Button"
 import NewTaskForm from "./components/modals/NewTaskForm"
 import Table from "./components/Table"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function App() {
-  const dummyData = [
-    {
-      description: 'Instalar solar wing',
-      status: {
-        percentage: 0,
-        label: 'EN PROCESO'
-      },
-      asignee: 'Vicente',
-    },
-    {
-      description: 'Conectar plogas',
-      status: {
-        percentage: 50,
-        label: 'EN PROCESO'
-      },
-      asignee: 'Mariano'
-    },
-    {
-      description: 'Instalar Faros 9\"',
-      status: {
-        percentage: 100,
-        label: 'COMPLETADO'
-      },
-      asignee: 'Adolfo'
-    }
-  ]
+
   const [showModal, setShowModal] = useState(false);
-  const [data, setData] = useState(dummyData);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  
+  useEffect(() => {
+    fetch(import.meta.env.VITE_SERVER_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching tasks:", error);
+        setLoading(false)
+      })
+  },[])
 
   function onAddTask(task){
-    console.log("added dummy")
-    console.log(task);
-    setData(dummyData => [...dummyData, task])
+    setData(apiData => [...apiData, task])
   }
 
+  if (loading) return <p>Loading...</p>;
+  
   return(
     <>
       <Table data={data}/>
